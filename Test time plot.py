e2aa77aa8e3e7b23588e01336146d7a039be5440
Nils <nils.tanneau@grenoble-inp.org>
2023-10-04 13:25:31 +0200

    Latest version, plot of BP and PAT

--- Test time plot.py
+++ Test time plot.py
@@ -5,7 +5,7 @@ Created on Wed Oct  4 10:58:04 2023
 @author: tanne
 """
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 import numpy as np 
@@ -20,3 +20,6 @@ for i in range(2343):
             data[i][j]=0
 X = data[1:, 1:4].astype(float)
 y = data[1: , 5].astype(float)
+fig1 = plt.figure()
+plt.subplot(2,1,1), plt.plot(X[:,0],X[:,2], label='PAT'),plt.ylabel("PAT")
+plt.subplot(2,1,2), plt.plot(X[:,0],y, 'g', label='BP'),plt.ylabel("Blood Pressure")
