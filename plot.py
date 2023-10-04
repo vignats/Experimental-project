cd8622195ca094f92a6c739d548978700812201b
Salomé Vignat <salome.vi38@gmail.com>
2023-10-04 15:56:30 +0200

    Autocorrelation

--- plot.py
+++ plot.py
@@ -8,13 +8,14 @@ import matplotlib.pyplot as plt
 
 import pandas as pd 
 import numpy as np 
-# read excel in data frame 
+## Read excel in data frame 
 df = pd.read_csv('03-Oct-2023_patAnalysis_2.csv') 
+
+## Data analysis
 time = df['wrist@(9mm,809nm)_delay_s']
 bp_systolic = df['blood pressure_systolic']
 pat_raw = df['wrist@(9mm,809nm)_raw_pat']
 pat_filtred = df['wrist@(9mm,809nm)_filtered_pat_bottomTI']
-
 
 # Plot 
 plt.figure(1)
@@ -27,3 +28,29 @@ plt.figure(3)
 plt.plot(time, pat_raw)
 plt.title('PAT raw')
 plt.show()
+
+## Mean and standard deviation
+# Global mean and standard deviation
+mean = df.mean()
+std = df.std()
+
+# Moving mean and standard deviation
+window = 60
+plt.figure(4)
+bp_systolic.rolling(window).mean().plot(style='k')
+plt.title('Moving mean')
+
+plt.figure(5)
+bp_systolic.rolling(window).std().plot(style='k')
+plt.title('Moving std')
+
+## Auto-correlation
+bp_s = bp_systolic.fillna(0)
+auto_correlation = np.correlate(bp_s.array, bp_s.array, mode='same')
+
+plt.figure(6)
+plt.plot(auto_correlation)
+plt.title('Autocorrelation')
+
+
+
