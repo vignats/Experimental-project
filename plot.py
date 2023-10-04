351ba607c1e55cde9327e2e1872c35ffb2da2e5a
Salomé Vignat <salome.vi38@gmail.com>
2023-10-04 14:07:40 +0200

    'plot_with_series'

---
+++ plot.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 11:48:59 2023
+
+@author: salome
+"""
+import matplotlib.pyplot as plt
+
+import pandas as pd 
+import numpy as np 
+## Read excel in data frame 
+df = pd.read_csv('03-Oct-2023_patAnalysis_2.csv') 
+
+## Data analysis
+time = df['wrist@(9mm,809nm)_delay_s']
+bp_systolic = df['blood pressure_systolic']
+pat_raw = df['wrist@(9mm,809nm)_raw_pat']
+pat_filtred = df['wrist@(9mm,809nm)_filtered_pat_bottomTI']
+
+# Plot 
+plt.figure(1)
+plt.plot(time, bp_systolic)
+plt.title('BP')
+plt.figure(2)
+plt.plot(time, pat_filtred)
+plt.title('PAT filtred')
+plt.figure(3)
+plt.plot(time, pat_raw)
+plt.title('PAT raw')
+plt.show()
+
+## Mean and standard deviation
+# Global mean and standard deviation
+mean = df.mean()
+std = df.std()
+
+# Moving mean and standard deviation
+window = 200
+plt.figure(4)
+bp_systolic.rolling(window).mean().plot(style='k')
+plt.title('Moving mean')
+
+plt.figure(5)
+bp_systolic.rolling(window).std().plot(style='k')
+plt.title('Moving std')
+
+
