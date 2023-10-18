30d71096029627da4fe1ff6fecf9801d44b0ddec
Nils <nils.tanneau@grenoble-inp.org>
2023-10-18 10:56:32 +0200

    simple file to use as a header for model usage

---
+++ Preprocessing before modeling.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 10:14:08 2023
+
+@author: tanne
+"""
+import pandas as pd 
+from scipy.interpolate import interp1d
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+
+def interpol(df, name_col):
+   time = df['wrist@(9mm,809nm)_delay_s']
+   data_squashed = df.dropna(subset=['wrist@(9mm,809nm)_filtered_pat_bottomTI'])
+   interp_func = interp1d(data_squashed['wrist@(9mm,809nm)_delay_s'].array, data_squashed['wrist@(9mm,809nm)_filtered_pat_bottomTI'].array, kind='linear', fill_value="extrapolate")
+   continuous_values = interp_func(time)
+   return continuous_values 
+    
+def Detrend(mat):
+    Diff = np.zeros(mat.shape)
+    Diff[0] = mat[0]
+    Diff[len(mat)-1]=Diff[len(mat)-1]
+    for i in range (1,len(mat)-1,1):
+        Diff[i]= np.abs(mat[i]-mat[i-1])
+    return Diff
+
+if __name__ == '__main__' :
+ df = pd.read_csv('03-Oct-2023_patAnalysis_2.csv')
+ name_col ='wrist@(9mm,809nm)_filtered_pat_bottomTI'
+ mat = interpol(df,name_col)
+ Diff = Detrend(mat)
+ X_Stationnary = Diff[23:2325]
+ X_raw = mat[23:2325]
+ y = df['blood pressure_systolic'].dropna().to_numpy()
