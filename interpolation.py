6e242c9b48105fb495c703880c4f77b39e5f7c63
Salomé Vignat <salome.vi38@gmail.com>
2023-10-18 13:19:50 +0200

    third_session

--- interpolation.py
+++ interpolation.py
@@ -4,17 +4,16 @@ Created on Wed Oct 11 15:22:25 2023
 
 @author: salome
 """
-import pandas as pd 
 from scipy.interpolate import interp1d
 
-## Read excel in data frame 
-df = pd.read_csv('03-Oct-2023_patAnalysis_2.csv')
-
-## Interpolation
-time = df['wrist@(9mm,809nm)_delay_s']
-data_squashed = df.dropna(subset=['wrist@(9mm,809nm)_filtered_pat_bottomTI'])
-
-interp_func = interp1d(data_squashed['wrist@(9mm,809nm)_delay_s'].array, data_squashed['wrist@(9mm,809nm)_filtered_pat_bottomTI'].array, kind='linear', fill_value="extrapolate")
-continuous_values = interp_func(time)
-
-df.insert(df.columns.get_loc('wrist@(9mm,809nm)_filtered_pat_bottomTI') + 1, 'pat_filtred_continuous', continuous_values) 
+def interpolation(df):
+ 
+    ## Interpolation
+    time = df['wrist@(9mm,809nm)_delay_s']
+    data_squashed = df.dropna(subset=['wrist@(9mm,809nm)_filtered_pat_bottomTI'])
+    
+    interp_func = interp1d(data_squashed['wrist@(9mm,809nm)_delay_s'].array, data_squashed['wrist@(9mm,809nm)_filtered_pat_bottomTI'].array, kind='linear', fill_value="extrapolate")
+    continuous_values = interp_func(time)
+    
+    df.insert(df.columns.get_loc('wrist@(9mm,809nm)_filtered_pat_bottomTI') + 1, 'pat_filtred_continuous', continuous_values) 
+    return df 
