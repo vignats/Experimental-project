bfc5a6aff0aaea359ae21683efbf9c2ca5c89101
Nils <nils.tanneau@grenoble-inp.org>
2023-11-29 16:16:44 +0100

    Je crois ça marche

--- Chatgptcode for autoregressive with TensorFlow.py
+++ Chatgptcode for autoregressive with TensorFlow.py
@@ -13,7 +13,8 @@ from math import sqrt
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from models import Preprocess
-
+from scipy import stats
+from scipy.interpolate import interp1d
 def summarize_diagnostics(history):
 	# plot loss
     plt.subplot(211)
@@ -30,27 +31,37 @@ def summarize_diagnostics(history):
 # Importing the data
 
 df = pd.read_csv('03-Oct-2023_patAnalysis_2.csv')
-df['wrist@(9mm,809nm)_delay_s'] = pd.to_datetime(df['wrist@(9mm,809nm)_delay_s'])
-df.set_index('wrist@(9mm,809nm)_delay_s', inplace=True)
-## Getting the data right with the correct index
-
+df = df.dropna(subset = ["blood pressure_systolic"])
 # Feature Engineering
-df_squashed = df.dropna(subset=['wrist@(9mm,809nm)_filtered_pat_bottomTI'])
-df_squashed = df_squashed.dropna(subset = ["blood pressure_systolic"])
+
 #Outliers removal 
-##TO BE COMPLETED##
+# Make a copy of the dataframe to fill in  NaN of PAT with mean NaN
+df_outlier = df.copy()
+df_outlier = df_outlier.dropna(subset = ["blood pressure_systolic"])
+df_outlier['wrist@(9mm,809nm)_filtered_pat_bottomTI'] = df_outlier['wrist@(9mm,809nm)_filtered_pat_bottomTI'].fillna(df_outlier['wrist@(9mm,809nm)_filtered_pat_bottomTI'].mean())
+
+# Commpute the z-statistic
+z = np.abs(stats.zscore(df_outlier['wrist@(9mm,809nm)_filtered_pat_bottomTI']))
+# Identify outliers as the pat_filtered with a z-score greater than 3
+threshold = 3
+outliers = df_outlier['wrist@(9mm,809nm)_filtered_pat_bottomTI'][z > threshold]
+df = df.drop(outliers.index)
 
 #Interpolation 
-p = Preprocess('03-Oct-2023_patAnalysis_2.csv')
-p.interpol()
-df_squashed_interpolled= p.df
+time_continuous = df['wrist@(9mm,809nm)_delay_s']
+time_not_continuous = df.loc[df['wrist@(9mm,809nm)_filtered_pat_bottomTI'].notna(), 'wrist@(9mm,809nm)_delay_s']
+interp_func = interp1d(time_not_continuous.array, df.loc[df['wrist@(9mm,809nm)_filtered_pat_bottomTI'].notna(), 'wrist@(9mm,809nm)_filtered_pat_bottomTI'].array, kind='linear', fill_value="extrapolate")
+continuous_values = interp_func(time_continuous)
+df.insert(loc=3, column='pat_filtred_continuous', value=continuous_values)
 
 #Dropping Nan Values
-#data_squashed = df.dropna(subset=['blood pressure_systolic'])
 
+## Getting the data right with the correct index
+df['wrist@(9mm,809nm)_delay_s'] = pd.to_datetime(df['wrist@(9mm,809nm)_delay_s'])
+df.set_index('wrist@(9mm,809nm)_delay_s', inplace=True)
 # Train-Test Split
-train_size = int(len(df_squashed_interpolled) * 0.7)
-train, test = df_squashed_interpolled[0:train_size], df_squashed_interpolled[train_size:]
+train_size = int(len(df['pat_filtred_continuous']) * 0.7)
+train, test = df[0:train_size], df[train_size:]
 
 
 # Normalize data using Standard scaling
@@ -68,13 +79,13 @@ def create_sequences(data, seq_length):
         targets.append(target)
     return np.array(sequences), np.array(targets)
 
-seq_length = 60  # Adjust as needed
+seq_length = 5  # Adjust as needed
 X_train, y_train = create_sequences(train_normalized, seq_length)
 X_test, y_test = create_sequences(test_normalized, seq_length)
 
 # Build a simple neural network
 model = tf.keras.Sequential([
-    tf.keras.layers.LSTM(50,activation="relu", input_shape=(X_train.shape[1], X_train.shape[2])),
+    tf.keras.layers.LSTM(25, activation="relu", input_shape=(X_train.shape[1], X_train.shape[2])),
     tf.keras.layers.Dense(1)
 ])
 
@@ -91,9 +102,10 @@ train_rmse = sqrt(mean_squared_error(y_train, train_predictions))
 test_rmse = sqrt(mean_squared_error(y_test, test_predictions))
 print("Training RMSE :", train_rmse, 'Testing RMSE : ', test_rmse)
 
+
 # Prediction
-future_steps = 5  # Adjust as needed
-future_data = test_normalized[-seq_length:].reshape((1, seq_length, 2))  # Assuming 2 features (PAT and BloodPressure)
+future_steps = 10  # Adjust as needed
+future_data = test_normalized[-seq_length:].reshape((1, 5, 2))  # Assuming 2 features (PAT and BloodPressure)
 future_predictions = []
 
 for _ in range(future_steps):
@@ -106,7 +118,7 @@ for _ in range(future_steps):
     future_predictions.append(prediction)
 
       
-# Concatenate 'PAT' values with future predictions for inverse transform
+# # Concatenate 'PAT' values with future predictions for inverse transform
 future_predictions_with_pat = np.column_stack((future_data[0, 0:future_steps, 0], np.array(future_predictions)))
 test_predictions_stacked = np.column_stack((test_normalized[seq_length:, 0],test_predictions))
 
@@ -116,12 +128,12 @@ test_predictions_denormalized = scaler.inverse_transform(test_predictions_stacke
 print('Predicted values for the next', future_steps ,'th step :', future_predictions_denormalized[:,1])
 
 # Plotting data
-plt.plot(train.index, train['blood pressure_systolic'], label='Train')
-plt.plot(test.index, test['blood pressure_systolic'], label='Test')
-plt.plot(test.index[seq_length:], test_predictions_denormalized[:,1], label='Test Predictions', color='red')
-plt.plot(pd.date_range(test.index[-1], periods=future_steps+1, freq='N')[1:], future_predictions_denormalized,
-         label='Future Predictions', color='green')
+plt.plot(range(0,len(train),1), train['blood pressure_systolic'], label='Train')
+plt.plot(range(len(train)+1,len(test)+len(train)+1,1), test['blood pressure_systolic'], label='Test')
+plt.plot(range(len(train)+1+seq_length,len(test)+len(train)+1,1), test_predictions_denormalized[:,1], label='Test Predictions', color='red')
+plt.plot(range(len(test)+len(train)+1,len(test)+len(train)+(len(future_predictions_denormalized)+1),1), future_predictions_denormalized[:,1], label='Future Predictions', color='green')
 plt.legend()
+plt.ylabel('Blood Pressure')
 plt.show()
 #Plotting performance curves
 summarize_diagnostics(history)
