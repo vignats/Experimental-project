eecd5be80eda43e13badf8494dd686e68743033d
Nils <nils.tanneau@grenoble-inp.org>
2023-12-06 10:31:31 +0100

    test

--- models.py
+++ models.py
@@ -14,6 +14,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split, TimeSeriesSplit
+from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 
